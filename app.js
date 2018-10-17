89eff59b274b561ebd2772e89e3a28d197086e52
Gonzalo Iniguez <gonzaini@Gonza.local>
2018-10-17 17:56:09 -0300

    getMateria()

--- app.js
+++ app.js
@@ -1,37 +1,51 @@
-var express = require("express");
+var bodyParser = require('body-parser');
+var express = require('express');
 var app = express();
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
 
+app.use(bodyParser.urlencoded({extend:true}));
+app.engine('html', require('ejs').renderFile);
+app.set('view engine', 'html');
+app.set('views', __dirname);
 app.use(express.static(__dirname + '/View'));
-//Store all HTML files in view folder.
-app.use(express.static(__dirname + '/Script'));
-//Store all JS and CSS in Scripts folder.
 
 
-app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
-
+//Posts
+app.post('/deploy',function(req,res){
+  //Codigo
+  TutoriaContract = new web3.eth.Contract(abiDefinition,{data: byteCode,address:'0000730bab2d10d2179aec947409e2f0c5d1ac5021' ,from: '0xfd730bab2d10d2179aec947409e2f0c5d1ac5021',gasPrice:'1000', gas: 6721975});
+  TutoriaContract.deploy({data:byteCode}).send({from:'0xfd730bab2d10d2179aec947409e2f0c5d1ac5021',gas: 6721975, gasPrice: '1000'});
+  res.redirect('/');
+})
 
 app.get('/',function(req,res){
-  res.sendFile('index.html');
+  res.sendFile(__dirname + '/View/index.html');
+  res.render()
 });
 
 app.post('/',function(req,res){
     let materia = req.body.materia;
     let profesor = req.body.profesor;
-    let smartcontractAddress = req.body.address;
-    myContract = new web3.eth.Contract(abiDefinition,smartcontractAddress, {data:byteCode,gasPrice:'1000',gas:200000});
+    let address = req.body.address;
+
+    myContract = new web3.eth.Contract(abiDefinition,address, {data:byteCode,gasPrice:'1000',gas:200000});
     myContract.methods.solicitar(materia,profesor).send({from:'0xfd730bab2d10d2179aec947409e2f0c5d1ac5021',gas:200000});
 
 })
 
-app.get('/about',function(req,res){
-  res.sendFile('/about.html');
+app.get('/getmateria',function(req,res){
+  
+  myContract.methods.getMateria('0xfd730bab2d10d2179aec947409e2f0c5d1ac5021').call().then(e => {
+    var respuesta = 'Materia: ';
+      
+
+      for (let index = 0; index < e.length; index++) {
+        const a = e[index];
+        respuesta += a.toString();
+      }    
+      
+      res.send(respuesta);
+  });
+  //res.sendFile(__dirname + '/View/getmateria.html',{materia:mate});
 });
 
 app.get('/sitemap',function(req,res){
@@ -43,8 +57,6 @@ Web3 = require('web3');
 fs = require('fs')
 solc = require('solc')
 web3 = new Web3(new Web3.providers.HttpProvider("http://localhost:8545"));
-var Accounts = require('web3-eth-accounts');
-var usuarios ;
 web3.eth.getAccounts(console.log);
 
 code = fs.readFileSync('./Script/tutoria.sol').toString() 
@@ -55,13 +67,14 @@ abiDefinition = JSON.parse(compiledCode.contracts[':Tutoria'].interface) //Su co
 
 byteCode = compiledCode.contracts[':Tutoria'].bytecode //Su contrato inteligente
 
-TutoriaContract = new web3.eth.Contract(abiDefinition,{data: byteCode, from: '0xfd730bab2d10d2179aec947409e2f0c5d1ac5021', gas: 6721975})
+//TutoriaContract = new web3.eth.Contract(abiDefinition,{data: byteCode, from: '0xfd730bab2d10d2179aec947409e2f0c5d1ac5021',gasPrice:'1000', gas: 6721975})
 
-TutoriaContract.deploy({data:byteCode}).send({from:'0xfd730bab2d10d2179aec947409e2f0c5d1ac5021',gas: 6721975, gasPrice: '1000'});
+//TutoriaContract.deploy({data:byteCode}).send({from:'0xfd730bab2d10d2179aec947409e2f0c5d1ac5021',gas: 6721975, gasPrice: '1000'});
 
 
 
-app.listen(3000);
+
+app.listen(3001);
 
 console.log("Running at Port 3000");
 
@@ -71,7 +84,3 @@ console.log("Running at Port 3000");
 
 
 
-
-function click(){
-    myContract.methods.getMateria().send({from:'0xfd730bab2d10d2179aec947409e2f0c5d1ac5021',gas:200000});
-}
