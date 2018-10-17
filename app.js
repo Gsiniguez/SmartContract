52d5c379f8276601fec3212cb7698995b4ef2517
Gonzalo Iniguez <gonzaini@Gonza.local>
2018-10-17 20:14:53 -0300

    Seleccionar usuario y profesor + metodos

--- app.js
+++ app.js
@@ -2,54 +2,185 @@ var bodyParser = require('body-parser');
 var express = require('express');
 var app = express();
 
-app.use(bodyParser.urlencoded({extend:true}));
+app.use(express.static(__dirname + '/View'));
+app.use(bodyParser.urlencoded({ extend: true }));
 app.engine('html', require('ejs').renderFile);
 app.set('view engine', 'html');
-app.set('views', __dirname);
-app.use(express.static(__dirname + '/View'));
 
+
+
+let config = {
+  addressContract: null
+};
 
 //Posts
-app.post('/deploy',function(req,res){
+app.post('/deploy', function (req, res) {
   //Codigo
-  TutoriaContract = new web3.eth.Contract(abiDefinition,{data: byteCode,address:'0000730bab2d10d2179aec947409e2f0c5d1ac5021' ,from: '0xfd730bab2d10d2179aec947409e2f0c5d1ac5021',gasPrice:'1000', gas: 6721975});
-  TutoriaContract.deploy({data:byteCode}).send({from:'0xfd730bab2d10d2179aec947409e2f0c5d1ac5021',gas: 6721975, gasPrice: '1000'});
-  res.redirect('/');
+  TutoriaContract = new web3.eth.Contract(abiDefinition, { data: byteCode, address: '0000730bab2d10d2179aec947409e2f0c5d1ac5021', from: '0xfd730bab2d10d2179aec947409e2f0c5d1ac5021', gasPrice: '1000', gas: 6721975 });
+  TutoriaContract.deploy({ data: byteCode }).send({ from: '0xfd730bab2d10d2179aec947409e2f0c5d1ac5021', gas: 6721975, gasPrice: '1000' }).then((e) => {
+    config.addressContract = e.options.address;
+    res.redirect('/');
+  });
 })
 
-app.get('/',function(req,res){
+app.get('/', function (req, res) {
   res.sendFile(__dirname + '/View/index.html');
   res.render()
 });
 
-app.post('/',function(req,res){
-    let usuario = req.body.usuario;
-    let materia = req.body.materia;
-    let profesor = req.body.profesor;
-    let address = req.body.address;
+app.post('/', function (req, res) {
+  let usuario = req.body.usuario;
+  let materia = req.body.materia;
+  let profesor = req.body.profesor;
+  if(profesor == usuario){
+    res.send("Usuario y Profesor no pueden ser el mismo.")
+  }
 
-    myContract = new web3.eth.Contract(abiDefinition,address, {data:byteCode,gasPrice:'1000',gas:200000});
-    myContract.methods.solicitar(materia,profesor).send({from:usuario,gas:200000});
+  myContract = new web3.eth.Contract(abiDefinition, config.addressContract, { data: byteCode, gasPrice: '1000', gas: 200000 });
+  myContract.methods.solicitar(materia, profesor).send({ from: usuario, gas: 200000 });
 
 })
 
-app.get('/getmateria',function(req,res){
-  
+app.get('/metodos', function (req, res) {
+  res.sendFile(__dirname + '/View/metodos.html');
+})
+
+app.post('/metodos', function (req, res) {
+  let usuario = req.body.usuario;
+  let metodo = req.body.metodo;
+  switch (metodo) {
+    case "1":
+      myContract.methods.getMateria(usuario).call().then(e => {
+
+        if (e.length < 1) {
+          res.send('Usuario Invalido')
+        }
+        var respuesta = 'RESPUESTA AL METODO getMateria(): ';
+        for (let index = 0; index < e.length; index++) {
+          const a = e[index];
+          respuesta += a.toString();
+        }
+        res.send(respuesta);
+      });
+      break;
+    case "2":
+      myContract.methods.getFecha(usuario).call().then(e => {
+
+        if (e.length < 1) {
+          res.send('Usuario Invalido')
+        }
+        var respuesta = 'RESPUESTA AL METODO getFecha(): ';
+        for (let index = 0; index < e.length; index++) {
+          const a = e[index];
+          respuesta += a.toString();
+        }
+        res.send(respuesta);
+      });
+      break;
+    case "3":
+      myContract.methods.getIdProfesor(usuario).call().then(e => {
+
+        if (e.length < 1) {
+          res.send('Usuario Invalido')
+        }
+        var respuesta = 'RESPUESTA AL METODO getIdProfesor(): ';
+        for (let index = 0; index < e.length; index++) {
+          const a = e[index];
+          respuesta += a.toString();
+        }
+        res.send(respuesta);
+      });
+      break;
+    case "4":
+      myContract.methods.getAlumno(usuario).call().then(e => {
+
+        if (e.length < 1) {
+          res.send('Usuario Invalido')
+        }
+        var respuesta = 'RESPUESTA AL METODO getAlumno(): ';
+        for (let index = 0; index < e.length; index++) {
+          const a = e[index];
+          respuesta += a.toString();
+        }
+        res.send(respuesta);
+      });
+      break;
+    case "5":
+      myContract.methods.confirmar(usuario).call().then(e => {
+
+        if (e.length < 1) {
+          res.send('Usuario Invalido')
+        }
+        var respuesta = 'RESPUESTA AL METODO confirmar(): ';
+        for (let index = 0; index < e.length; index++) {
+          const a = e[index];
+          respuesta += a.toString();
+        }
+        res.send(respuesta);
+      });
+      break;
+    case "6":
+      myContract.methods.cancelar(usuario).call().then(e => {
+
+        if (e.length < 1) {
+          res.send('Usuario Invalido')
+        }
+        var respuesta = 'RESPUESTA AL METODO cancelar(): ';
+        for (let index = 0; index < e.length; index++) {
+          const a = e[index];
+          respuesta += a.toString();
+        }
+        res.send(respuesta);
+      });
+      break;
+    case "7":
+      myContract.methods.estaConfirmado(usuario).call().then(e => {
+
+        if (e.length < 1) {
+          res.send('Usuario Invalido')
+        }
+        var respuesta = 'RESPUESTA AL METODO estaConfirmado(): ';
+        for (let index = 0; index < e.length; index++) {
+          const a = e[index];
+          respuesta += a.toString();
+        }
+        res.send(respuesta);
+      });
+      break;
+    case "8":
+      myContract.methods.estaCancelado(usuario).call().then(e => {
+
+        if (e.length < 1) {
+          res.send('Usuario Invalido')
+        }
+        var respuesta = 'RESPUESTA AL METODO estaCancelado(): ';
+        for (let index = 0; index < e.length; index++) {
+          const a = e[index];
+          respuesta += a.toString();
+        }
+        res.send(respuesta);
+      });
+      break;
+  }
+})
+
+app.get('/getmateria', function (req, res) {
+
   myContract.methods.getMateria('0xfd730bab2d10d2179aec947409e2f0c5d1ac5021').call().then(e => {
     var respuesta = 'Materia: ';
-      
 
-      for (let index = 0; index < e.length; index++) {
-        const a = e[index];
-        respuesta += a.toString();
-      }    
-      
-      res.send(respuesta);
+
+    for (let index = 0; index < e.length; index++) {
+      const a = e[index];
+      respuesta += a.toString();
+    }
+
+    res.send(respuesta);
   });
   //res.sendFile(__dirname + '/View/getmateria.html',{materia:mate});
 });
 
-app.get('/sitemap',function(req,res){
+app.get('/sitemap', function (req, res) {
   res.sendFile('/sitemap.html');
 });
 
@@ -60,7 +191,7 @@ solc = require('solc')
 web3 = new Web3(new Web3.providers.HttpProvider("http://localhost:8545"));
 web3.eth.getAccounts(console.log);
 
-code = fs.readFileSync('./Script/tutoria.sol').toString() 
+code = fs.readFileSync('./Script/tutoria.sol').toString()
 
 compiledCode = solc.compile(code)
 
